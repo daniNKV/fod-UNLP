1a2d557297b5df914439453cf9a903492e465974
daniNKV <nikiforovdaniel@gmail.com>
2023-03-24 21:07:18 -0300

    Crear archivo con y sin entrada (5) a))

--- practica1/ejercicio5y6.pas
+++ practica1/ejercicio5y6.pas
@@ -32,11 +32,12 @@
 // NOTA: Las búsquedas deben realizarse por nombre de celular
 
 program ejercicio5y6;
+const fin_archivo = -1;
 type
     texto_corto = string[20];
     texto_largo = string[100];
     
-    celular = record
+    tipoCelular = record
         codigo: integer;
         nombre: texto_corto;
         marca: texto_corto;
@@ -48,6 +49,93 @@ type
 
     archivo_celulares = file of celular;
 
+procedure pedirCelular(var c: tipoCelular);
+begin
+	writeln('Ingrese codigo: ');
+	readln(c.apellido);
+	if (c.codigo <> '-1') then begin
+		writeln('Ingrese nombre:');
+		readln(c.nombre);
+		writeln('Ingrese marca: ');
+		readln(c.marca);
+		writeln('Ingrese descripcion: ');
+		readln(c.descripcion);
+		writeln('Ingrese precio: ');
+		readln(c.precio);
+		writeln('Ingrese stock minimo: ');
+		readln(c.stock_minimo);
+		writeln('Ingrese stock disponible: ');
+		readln(c.stock_disponible);
+	end;
+end;
+
+
+procedure leerCelular(var archivo: archivo_celulares; var celular: tipoCelular);
+begin
+    if (not EOF(archivo)) then
+        read(archivo, celular)
+    else
+        celular.codigo := fin_archivo;
+end;
+
+
+procedure crearArchivo();
+var
+    celular: tipoCelular;
+    archivo: archivo_celulares;
+    nombre_archivo: texto_corto;
+begin   
+    write('Ingrese nombre del nuevo archivo: ');
+    readln(nombre_archivo);
+
+    assign(archivo, nombre_archivo);
+    rewrite(archivo);
+
+    pedirCelular(celular);
+    
+    while(celular.codigo <> fin_archivo) do begin
+        write(archivo, celul);
+        pedirCelular(celular);
+    end;
+    
+    close(archivo);
+end;
+
+
+procedure crearDesdeArchivo();
+var
+    nombre_fuente: texto_corto;
+    fuente: archivo_celulares;
+    
+    nuevo_nombre: texto_corto;
+    nuevo_archivo: archivo_celulares;
+
+    celular_leido: tipoCelular;
+    
+begin
+    nombre_fuente := 'celulares.txt';
+
+    write('Ingrese nombre del nuevo archivo: ');
+    writeln(nuevo_nombre);
+
+    assign(nuevo_archivo, nuevo_nombre);
+    assign(fuente, nombre_fuente);
+
+    reset(fuente);
+    rewrite(nuevo_archivo);
+
+    leerCelular(fuente, celularLeido);
+
+    while (celularLeido.codigo <> fin_archivo) do begin
+        write(nuevo_archivo, celular_leido);
+        leerCelular(fuente, celular_leido);
+    end;
+
+    close(fuente);
+    close(nuevo_archivo);
+end;
+
+
 
 var
     opcion_elegida: char;
@@ -57,6 +145,7 @@ begin
     writeln('-----------------------------------');
     writeln('############ CELULARES ############');
     writeln('-----------------------------------');
+    writeln('Z: Crear Archivo');
     writeln('A: Crear archivo de celulares"');
     writeln('B: Listar productos con stock menor al mínimo');
     writeln('C: Buscar por descripción');
@@ -70,13 +159,14 @@ begin
     readln(opcion_elegida);
 
     case opcion_elegida of 
-        'A', 'a': crearArchivo();
-        'B', 'b': listarBajoStock();
-        'C', 'c': buscarPorDescripcion();
-        'D', 'd': exportarTodo();
-        'E', 'e': agregarCelulares();
-        'F', 'f': modificarStock();
-        'G', 'g': exportarSinStock(); 
+        'Z', 'z': crearArchivo();
+        'A', 'a': crearDesdeArchivo();
+        // 'B', 'b': listarBajoStock();
+        // 'C', 'c': buscarPorDescripcion();
+        // 'D', 'd': exportarTodo();
+        // 'E', 'e': agregarCelulares();
+        // 'F', 'f': modificarStock();
+        // 'G', 'g': exportarSinStock(); 
     end;
 
 end.
