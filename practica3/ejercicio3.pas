93924832b4850ca7da32d921a22ebd818f4e36d0
daniNKV <nikiforovdaniel@gmail.com>
2023-05-10 15:09:49 -0300

    Implementa crear archivos y dar altas ej3

---
+++ practica3/ejercicio3.pas
@@ -0,0 +1,165 @@
+// 3. Realizar un programa que genere un archivo de novelas filmadas durante el presente
+// año. De cada novela se registra: código, género, nombre, duración, director y precio.
+// El programa debe presentar un menú con las siguientes opciones:
+    // a. Crear el archivo y cargarlo a partir de datos ingresados por teclado. Se
+    // utiliza la técnica de lista invertida para recuperar espacio libre en el
+    // archivo. Para ello, durante la creación del archivo, en el primer registro del
+    // mismo se debe almacenar la cabecera de la lista. Es decir un registro
+    // ficticio, inicializando con el valor cero (0) el campo correspondiente al
+    // código de novela, el cual indica que no hay espacio libre dentro del
+    // archivo.
+    // b. Abrir el archivo existente y permitir su mantenimiento teniendo en cuenta el
+    // inciso a., se utiliza lista invertida para recuperación de espacio. En
+    // particular, para el campo de  ́enlace ́ de la lista, se debe especificar los
+    // números de registro referenciados con signo negativo, (utilice el código de
+    // novela como enlace).Una vez abierto el archivo, brindar operaciones para:
+        // i. Dar de alta una novela leyendo la información desde teclado. Para
+        // esta operación, en caso de ser posible, deberá recuperarse el
+        // espacio libre. Es decir, si en el campo correspondiente al código de
+        // novela del registro cabecera hay un valor negativo, por ejemplo -5,
+        // se debe leer el registro en la posición 5, copiarlo en la posición 0
+        // (actualizar la lista de espacio libre) y grabar el nuevo registro en la
+        // posición 5. Con el valor 0 (cero) en el registro cabecera se indica
+        // que no hay espacio libre.
+        // ii. Modificar los datos de una novela leyendo la información desde
+        // teclado. El código de novela no puede ser modificado.
+        // iii. Eliminar una novela cuyo código es ingresado por teclado. Por
+        // ejemplo, si se da de baja un registro en la posición 8, en el campo
+        // código de novela del registro cabecera deberá figurar -8, y en el
+        // registro en la posición 8 debe copiarse el antiguo registro cabecera.
+    // c. Listar en un archivo de texto todas las novelas, incluyendo las borradas, que
+    // representan la lista de espacio libre. El archivo debe llamarse “novelas.txt”.
+// NOTA: Tanto en la creación como en la apertura el nombre del archivo debe ser
+// proporcionado por el usuario
+
+program ejercicio3;
+const
+    FIN_ARCHIVO = 32767;
+type
+    cadena: string[40];
+    tipo_novela = record
+        codigo: integer;
+        genero: cadena;
+        nombre: cadena;
+        duracion: double;
+        director: cadena;
+        precio: double;
+    end;
+    archivo_novelas = file of tipo_novela;
+
+
+procedure LeerNovelaDeTeclado(var n: tipo_novela);
+begin
+    Write('Ingrese codigo: ');
+    readln(n.codigo);
+    if (n.codigo <> -1) then begin
+        Write('Ingrese genero: ');
+        readln(n.genero);
+        Write('Ingrese nombre: ');
+        readln(n.nombre);
+        Write('Ingrese duracion: ');
+        readln(n.duracion);
+        Write('Ingrese director: ');
+        readln(n.director);
+        Write('Ingrese precio: ');
+        readln(n.precio);
+    end;
+end;
+
+procedure LeerNovelaDeArchivo(var a: archivo_novelas; var n: tipo_novela);
+begin
+    if (not EOF(a)) then
+        read(a, n)
+    else
+        n.codigo := FIN_ARCHIVO;
+end;
+procedure CrearArchivo();
+var
+    archivo: archivo_novelas;
+    nombre_archivo: string;
+    cabecera: tipo_novela;
+    novela: tipo_novela;
+begin
+    write('Ingrese nombre del archivo a crearse: ');
+    readln(nombre_archivo);
+    assign(archivo, nombre_archivo);
+    rewrite(archivo);
+    cabecera.codigo := 0;
+    write(archivo, cabecera);
+
+    LeerNovelaDeTeclado(novela);
+
+    while (novela.codigo <> -1) do begin
+        write(archivo, novela);
+        LeerNovelaDeTeclado(novela;)
+    end;
+
+    close(archivo);
+end;
+
+
+
+procedure DarAlta(var a: archivo_novelas);
+var
+    novelaIngresada: tipo_novela;
+    novelaLeida: tipo_novela;
+begin
+    LeerNovelaDeTeclado(novelaIngresada);
+    
+    LeerNovelaDeArchivo(a, novelaLeida);
+    if (novelaLeida.codigo < 0) and (novelaLeida.codigo <> FIN_ARCHIVO) then begin
+        seek(a, Abs(novelaLeida.codigo));
+        read(a, novelaLeida);
+        seek(a, filePos(a) - 1);
+        write(a, novelaIngresada);
+        seek(a, 0);
+        write(a, novelaLeida);
+    end
+    else begin
+        seek(a, FileSize(a));
+        write(a, novelaIngresada);
+    end;
+
+
+end;
+
+procedure AbrirArchivo();
+var
+    archivo: archivo_novelas;
+    nombre_archivo: string;
+    opcion: char;
+begin
+    write('Ingrese nombre del archivo a modificar: ');
+    readln(nombre_archivo);
+    assign(archivo, nombre_archivo);
+    reset(archivo);
+
+    writeln('############### Menu Archivo #################');
+    writeln('A: Dar de alta una novela');   
+    writeln('B: Modificar novela');
+    writeln('C: Eliminar una novela por código');
+    writeln('##########################################');
+    write('Teclee una opción: ');
+    readln(opcion);
+
+    case opcion of 
+        'A', 'a': DarAlta(archivo)
+
+    end;
+
+    close(archivo);
+end;
+var
+    opcion: char;
+begin
+    writeln('############### Menu Principal #################');
+    writeln('A: Crear Archivo');
+    writeln('D: Modificar Archivo');
+    writeln('E: Listar Archivo');
+    writeln('##########################################');
+    write('Teclee una opción: ');
+    readln(opcion);
+
+    case opcion of 
+        'A', 'a': CrearArchivo();
+end.
